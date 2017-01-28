060224c6abe7efc63f1bf83747afb4036edf179a
Jesus <jofranco@bu.edu>
2017-01-27 23:37:21 -0500

    table view

--- CalculateInOne/TableViewCell1.swift
+++ CalculateInOne/TableViewCell1.swift
@@ -9,16 +9,8 @@
 import UIKit
 
 class TableViewCell1: UITableViewCell {
-
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
-    }
-
-    override func setSelected(_ selected: Bool, animated: Bool) {
-        super.setSelected(selected, animated: animated)
-
-        // Configure the view for the selected state
-    }
     
+    @IBOutlet var mainImageView: UIImageView!
+    
+    @IBOutlet var mainLabelView: UILabel!
 }
--- CalculateInOne/ViewController.swift
+++ CalculateInOne/ViewController.swift
@@ -8,9 +8,63 @@
 
 import UIKit
 
+struct Data{
+    let cell : Int!
+    let text : String!
+    let image : UIImage!
+}
+
 class TableViewController: UITableViewController{
+    
+    var array = [Data]()
+    
+    override func viewDidLoad() {
+        array = [Data(cell : 1, text : "Test", image : #imageLiteral(resourceName: "Screen Shot 2017-01-27 at 1.06.05 AM")),
+                 Data(cell : 2, text : "Test1", image : #imageLiteral(resourceName: "Screen Shot 2017-01-27 at 1.06.05 AM")),
+                 Data(cell : 1, text : "Test2", image : #imageLiteral(resourceName: "Screen Shot 2017-01-27 at 1.06.05 AM"))]
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        if array[indexPath.row].cell == 1{
+            let cell = Bundle.main.loadNibNamed("TableViewCell1", owner: self, options: nil)?.first as! TableViewCell1
+            cell.mainImageView.image = array[indexPath.row].image
+            cell.mainLabelView.text = array[indexPath.row].text
+            
+            return cell
+        }
+        else if array[indexPath.row].cell == 2{
+            let cell = Bundle.main.loadNibNamed("TableViewCell2", owner: self, options: nil)?.first as! TableViewCell2
+            cell.mainImageView.image = array[indexPath.row].image
+            cell.mainTableView.text = array[indexPath.row].text
+            
+            return cell
+        }
+        else {
+            let cell = Bundle.main.loadNibNamed("TableViewCell2", owner: self, options: nil)?.first as! TableViewCell1
+            cell.mainImageView.image = array[indexPath.row].image
+            cell.mainLabelView.text = array[indexPath.row].text
+            
+            return cell
+        }
+    }
 
-
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        if array[indexPath.row].cell == 1{
+            return 218
+        }
+        else if array[indexPath.row].cell == 2{
+            return 72
+        }
+        
+        else {
+            return 218
+        }
+        
+    }
 
 }
 
