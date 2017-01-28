74a44498a0cd8971c7a7ff9396b2d9942aabee13
Jesus <jofranco@bu.edu>
2017-01-27 19:30:06 -0500

    Personal Calculator

---
+++ CalculateInOne/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CalculateInOne
+//
+//  Created by Jesus Franco on 1/27/17.
+//  Copyright © 2017 Jesus Franco. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
