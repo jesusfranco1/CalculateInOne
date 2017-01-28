c0041a7fff23ac05c6fda12ef5b0df79d0e10511
Jesus <jofranco@bu.edu>
2017-01-27 21:20:46 -0500

    changes

---
+++ CalculateInOne/TableViewCell1.swift
@@ -0,0 +1,24 @@
+//
+//  TableViewCell1.swift
+//  CalculateInOne
+//
+//  Created by Jesus Franco on 1/27/17.
+//  Copyright © 2017 Jesus Franco. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell1: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+    
+}
---
+++ CalculateInOne/TableViewCell2.swift
@@ -0,0 +1,17 @@
+//
+//  TableViewCell2.swift
+//  CalculateInOne
+//
+//  Created by Jesus Franco on 1/27/17.
+//  Copyright © 2017 Jesus Franco. All rights reserved.
+//
+
+import UIKit
+
+class TableViewCell2: UITableViewCell {
+
+    @IBOutlet var mainImageView: UIImageView!
+  
+    @IBOutlet var mainTableView: UILabel!
+    
+}
